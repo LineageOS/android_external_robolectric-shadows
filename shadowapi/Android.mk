2d18cdc34a0912b5906abaa273cde0b8d69dd557
Colin Cross <ccross@android.com>
2019-04-23 15:11:07 -0700

    Convert robolectric libraries to Android.bp
    See build/soong/README.md for more information.
    
    Bug: 122331577
    Test: mmma external/robolectric-shadows
    Test: atest --host Robolectric_utils_tests
    Change-Id: Ib4d7ea5e11998a177eeb7474cc67b785d47d24a4
    Merged-In: Ib4d7ea5e11998a177eeb7474cc67b785d47d24a4
    (cherry picked from commit ea55ada29fa6d0917997bb5dc9dd6eaf17c1567d)

--- shadowapi/Android.mk
+++ shadowapi/Android.mk
@@ -1,33 +1,4 @@
-##############################################
-# Compile Robolectric shadowapi
-##############################################
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := Robolectric_shadowapi
-
-LOCAL_STATIC_JAVA_LIBRARIES := jsr305
-
-LOCAL_SRC_FILES := $(call all-java-files-under, src/main/java)
-
-include $(BUILD_HOST_JAVA_LIBRARY)
-
-##############################################
-# Compile Robolectric shadowapi tests
-##############################################
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := Robolectric_shadowapi_tests
-
-LOCAL_SRC_FILES := $(call all-java-files-under, src/test/java)
-
-LOCAL_STATIC_JAVA_LIBRARIES := \
-  Robolectric_shadowapi \
-  robolectric-assertj-core-3.8.0 \
-  hamcrest \
-  junit
-
-include $(BUILD_HOST_JAVA_LIBRARY)
 
 ##############################################
 # Execute Robolectric shadowapi tests
