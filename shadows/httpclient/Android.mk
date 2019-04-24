09a8e58924b0d923c765482f5a986e745537bd02
Colin Cross <ccross@android.com>
2019-04-24 14:53:04 -0700

    Don't use robolectric-host-org_apache_http_legacy in test runtime
    robolectric-host-org_apache_http_legacy only has stubs, it shouldn't
    be used as a runtime dependency.
    
    Bug: 122331577
    Test: m Run_robolectric_shadows_httpclient_tests
    Change-Id: Ibfcabf782c811d4fd6958f1dc305ec3064dd014a

--- shadows/httpclient/Android.mk
+++ shadows/httpclient/Android.mk
@@ -13,7 +13,6 @@ test_resources_directory := $(LOCAL_PATH)/src/test/resources
 
 test_runtime_libraries := \
   Robolectric_shadows_httpclient_tests \
-  robolectric-host-android_all \
-  robolectric-host-org_apache_http_legacy
+  robolectric-host-android_all
 
 include external/robolectric-shadows/run_robolectric_module_tests.mk
